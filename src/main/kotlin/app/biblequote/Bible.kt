1b25e95e72c2cb72bc23457021278f30c3fccfd6
Daniel Demidko <demidko@farpost.com>
2022-08-02 02:11:13 +0500

    Плоская структура хранения текста со смещениями

--- src/main/kotlin/app/biblequote/Bible.kt
+++ src/main/kotlin/app/biblequote/Bible.kt
@@ -3,18 +3,25 @@ package app.biblequote
 import java.net.URL
 
 class Bible(url: URL) {
+  private val booksNames = mutableListOf<String>()
+  private val versesTexts = mutableListOf<String>()
+  private val chaptersOffsets = mutableListOf<Int>()
+  private val booksOffsets = mutableListOf<Int>()
+
 
   init {
     val reader = url.openStream().bufferedReader().let(::BibleReader)
     reader.use {
       while (reader.hasNext) {
-        reader.nextVerse()
+
       }
     }
+
   }
 
   fun listAvailableBooks(): List<String> {
-
     TODO()
   }
+
+  fun lo
 }
