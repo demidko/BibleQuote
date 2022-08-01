d88c72e8dc7266191d77f454e7a543c05ab58b5a
Daniel Demidko <demidko@farpost.com>
2022-08-02 00:45:48 +0500

    Наработки для кеширующего класса

--- src/main/kotlin/app/biblequote/App.kt
+++ src/main/kotlin/app/biblequote/App.kt
@@ -2,9 +2,16 @@ package app.biblequote
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class App
+class App {
+
+  @Bean
+  fun textUrl() = javaClass.getResource("/bible.html")!!
+
+  fun bible() = Bible(textUrl())
+}
 
 fun main(args: Array<String>) {
   runApplication<App>(*args)
---
+++ src/main/kotlin/app/biblequote/Bible.kt
@@ -0,0 +1,15 @@
+package app.biblequote
+
+import java.net.URL
+
+class Bible(url: URL) {
+
+  init {
+    val reader = url.openStream().bufferedReader().let(::BibleReader)
+    reader.use {
+      while (reader.hasNext) {
+        reader.nextVerse()
+      }
+    }
+  }
+}
