1fae85c6a5c3f3f23a65f88cfbc0f2fec1ee5fa1
Daniel Demidko <demidko@farpost.com>
2023-02-01 21:10:39 +0500

    Вернул красивые фавиконки

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,26 +1,27 @@
 // noinspection JSUnresolvedFunction
 
 const MiniCss = require("mini-css-extract-plugin");
-const RemovePlugin = require('remove-files-webpack-plugin');
+const RemovePlugin = require("remove-files-webpack-plugin");
 const webpack = require("webpack");
 
 module.exports = {
   plugins: [
     new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
+      $: "jquery",
+      jQuery: "jquery",
     }),
     new MiniCss(),
     new RemovePlugin({
       before: {
         test: [
           {
-            folder: './dist',
+            folder: "./dist",
             method: () => true
           }
         ],
         exclude: [
-          './dist/index.html',
+          "./dist/index.html",
+          "./dist/favicon",
         ]
       }
     })
